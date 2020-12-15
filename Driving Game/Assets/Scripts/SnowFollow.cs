72a7cfba2dde57e6c81203af9a8a074c2780e0b3
marcus-elia <mselia@uvm.edu>
2020-12-14 19:04:05 -0500

    Added trees and snow

---
+++ Driving Game/Assets/Scripts/SnowFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowFollow : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private Transform target;
+
+    private void FixedUpdate()
+    {
+        HandleTranslation();
+    }
+
+    private void HandleTranslation()
+    {
+        var targetPosition = target.TransformPoint(offset);
+        transform.position = targetPosition;
+    }
+}
